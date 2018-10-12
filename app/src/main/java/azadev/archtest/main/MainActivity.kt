af93afd065a2bd2a766e1d4535c2ee7571830d93
Oleg Cherr <olegcherr@gmail.com>
2018-10-12 12:10:29 +0400

    Initial commit

---
+++ app/src/main/java/azadev/archtest/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package azadev.archtest.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import azadev.archtest.R
+
+class MainActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.main_activity)
+	}
+}
