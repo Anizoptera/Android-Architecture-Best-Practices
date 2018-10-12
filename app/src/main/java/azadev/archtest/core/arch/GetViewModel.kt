e303a97fe03eb4740d9507e1364232348f252720
Oleg Cherr <olegcherr@gmail.com>
2018-10-12 23:58:12 +0400

    Implement GetViewModel delegate helper

---
+++ app/src/main/java/azadev/archtest/core/arch/GetViewModel.kt
@@ -0,0 +1,30 @@
+package azadev.archtest.core.arch
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import kotlin.reflect.KProperty
+
+class GetViewModel<out T : ViewModel>(private val clazz: Class<T>) {
+
+	private var value: T? = null
+
+	operator fun getValue(thisRef: FragmentActivity, property: KProperty<*>): T {
+		if (value == null) {
+			value = ViewModelProviders.of(thisRef).get(clazz)
+		}
+
+		return value!!
+	}
+
+	operator fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+		if (value == null) {
+			value = ViewModelProviders.of(thisRef).get(clazz)
+		}
+
+		return value!!
+	}
+}
+
+inline fun <reified T : ViewModel> viewModel() = GetViewModel(T::class.java)
--- app/src/main/java/azadev/archtest/feat/form/FormActivity.kt
+++ app/src/main/java/azadev/archtest/feat/form/FormActivity.kt
@@ -1,23 +1,22 @@
 package azadev.archtest.feat.form
 
-import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import azadev.archtest.R
+import azadev.archtest.core.arch.viewModel
 import azadev.archtest.core.databinding.contentView
 import azadev.archtest.databinding.FormActivityBinding
 
 class FormActivity : AppCompatActivity(), FormPresenter.FormPresenterView {
 
 	private val binding by contentView<FormActivityBinding>(R.layout.form_activity)
-	private lateinit var model: FormViewModel
+	private val model by viewModel<FormViewModel>()
 	private lateinit var presenter: FormPresenter
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
-		model = ViewModelProviders.of(this)[FormViewModel::class.java]
 		presenter = FormPresenter(this, model)
 
 		binding.setLifecycleOwner(this)
