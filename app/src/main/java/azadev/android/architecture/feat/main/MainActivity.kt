24365ba55e735700142c1a6f663ff03201f1ec0f
Oleg Cherr <olegcherr@gmail.com>
2018-10-14 14:48:53 +0400

    MainActivity:  Use 'contentView' helper for binding

--- app/src/main/java/azadev/android/architecture/feat/main/MainActivity.kt
+++ app/src/main/java/azadev/android/architecture/feat/main/MainActivity.kt
@@ -1,20 +1,20 @@
 package azadev.android.architecture.feat.main
 
 import android.content.Intent
-import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import azadev.android.architecture.R
+import azadev.android.architecture.core.databinding.livedata.contentView
 import azadev.android.architecture.databinding.MainActivityBinding
 import azadev.android.architecture.feat.part1.Part1Activity
 import azadev.android.architecture.feat.part2.Part2Activity
 
 class MainActivity : AppCompatActivity(), MainPresenter {
 
+	private val binding by contentView<MainActivityBinding>(R.layout.main_activity)
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-
-		val binding = DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.main_activity)
 
 		binding.presenter = this
 	}
